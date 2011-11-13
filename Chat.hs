630cbb441ef466f48ff1cb58648a771c9a57265f
yihuang <yi.codeplayer@gmail.com>
2011-11-13 12:19:50 +0800

    base chatroom and tcp server interface.

---
+++ Chat.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Chat where
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+import Control.Applicative
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM
+import Control.Monad.State
+
+modifyTVar :: TVar a -> (a -> a) -> STM ()
+modifyTVar v f = readTVar v >>= writeTVar v . f
+
+liftSTM :: STM a -> ServerM a
+liftSTM = liftIO . atomically
+
+getState :: ServerM ServerState
+getState = get >>= liftSTM . readTVar
+
+modifyState :: (ServerState -> ServerState) -> ServerM ()
+modifyState f = get >>= liftSTM . flip modifyTVar f
+
+type Identity = Text
+type Message = Text
+
+type Transport = (TChan Message, TChan Message)
+
+data Client = Client
+  { identity :: Identity
+  , transport :: Transport
+  }
+
+type Clients = Map Identity Client
+type ServerState = Map Identity Client
+type TServerState = TVar ServerState
+
+type ServerM = StateT TServerState IO
+
+runServerM = evalStateT
+
+newServerState :: IO TServerState
+newServerState = newTVarIO M.empty
+
+newClient :: Identity -> IO Client
+newClient identity = Client <$> pure identity
+                            <*> ( (,) <$> newTChanIO <*> newTChanIO)
+
+numClients :: ServerM Int
+numClients = M.size <$> getState
+
+addClient :: Identity -> ServerM Transport
+addClient identity = do
+    client <- liftIO $ newClient identity
+    modifyState (M.insert identity client)
+    return $ transport client
+
+getClient :: Identity -> ServerM (Maybe Client)
+getClient identity = M.lookup identity <$> getState
+
+broadcast :: Message -> ServerM ()
+broadcast msg = do
+    chans <- map snd . map transport . M.elems <$> getState
+    liftIO $ mapM_ (atomically . flip writeTChan msg) chans
+
+testClient :: Identity -> Message -> ServerM ()
+testClient identity msg = do
+    (chanReq, chanRsp) <- addClient identity
+    liftIO $ forkIO $ forever $ do
+        s <- atomically (readTChan chanRsp)
+        T.putStrLn $ T.concat [identity, ":", s]
+    broadcast msg
+
+main = do
+    runServerM $ forM_ [1..10] $ \i -> testClient (T.pack $ "id"++show i) (T.pack $ "hello from"++show i)
+    return ()
---
+++ TcpServer.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.Attoparsec.Char8 as A
+import qualified Data.Text.Encoding as T
+import Data.Text (Text)
+import Blaze.ByteString.Builder (Builder, fromByteString)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as S
+
+import Control.Monad
+import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Monad.IO.Class
+
+import Network.Server.ScalableServer
+import Chat hiding (main)
+
+type Request = (ByteString, Text, Text)
+
+parseRequest :: A.Parser Request
+parseRequest = (,,) <$> A.takeTill A.isSpace <* A.skipWhile (==' ')
+                    <*> (T.decodeUtf8 <$> A.takeTill A.isSpace)
+                        <* A.skipWhile (==' ')
+                    <*> (T.decodeUtf8 <$> A.takeTill (A.inClass "\r\n"))
+                        <* A.string "\r\n"
+
+authenticated :: Request -> Client -> ServerM Text
+authenticated (cmd, identity, msg) client  = case cmd of
+    "broadcast" -> broadcast msg >> return "success.\n"
+    "recv" -> let (_, tranRsp) = transport client
+              in  liftIO (atomically (readTChan tranRsp)) >>= return
+
+handle :: Request -> ServerM Text
+handle ("join", identity, _) = addClient identity >> return "success.\n"
+handle req@(_, identity, _) = do
+    mc <- getClient identity
+    case mc of
+        Nothing -> return "not exist.\n"
+        Just c -> authenticated req c
+
+handler :: TServerState -> Request -> IO Builder
+handler st req = fromByteString . T.encodeUtf8 <$> runServerM (handle req) st
+
+main = do
+    st <- newServerState
+    let definition = RequestPipeline parseRequest (handler st) 10
+    _ <- forkIO $ runServer definition 6004
+    forever $ threadDelay (1000000 * 60)
