57317a51e236a0cb13285291604e93e71edecc89
yihuang <yi.codeplayer@gmail.com>
2011-11-15 12:49:12 +0800

    runnable

--- websocket.hs
+++ websocket.hs
@@ -1,89 +1,125 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Network.WebSockets hiding (fromLazyByteString)
-import qualified Network.WebSockets.Protocol as I
+{-# LANGUAGE OverloadedStrings, TupleSections #-}
+import System.IO.Unsafe
 import Data.Map (Map)
+import Data.List (stripPrefix)
 import qualified Data.Map as M
+import Data.Unique
 
+import Data.Text (Text)
+import qualified Data.Text.Encoding as T
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as S
 import qualified Data.ByteString.Lazy as L
-import Data.Enumerator
-import Data.Attoparsec
+import Data.Enumerator hiding (map, filter)
 import Blaze.ByteString.Builder
 
+import Control.Exception (throw)
 import Control.Monad.IO.Class
 import Control.Monad
 import Control.Applicative
 import Control.Concurrent
-import Control.Concurrent.Chan
-import Control.Concurrent.MVar
 
-type StreamChan a = Chan (Stream a)
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp (runSettings, defaultSettings, Settings(..))
+import Network.WebSockets hiding (Request, Response)
+import qualified Network.WebSockets as WS
+import Network.WebSockets.Chan
+import qualified Network.Wai.Handler.WebSockets as WaiWS
 
-iterChan :: StreamChan a -> Iteratee a IO ()
-iterChan ch = continue go
-  where
-    go EOF = liftIO $ writeChan ch EOF
-    go stream = liftIO (writeChan ch stream) >> continue go
+responseText :: Text -> Response
+responseText txt = ResponseBuilder statusOK [("Content-Type", "text/plain")] (fromByteString . T.encodeUtf8 $ txt)
 
-enumChan :: StreamChan a -> Enumerator a IO b
-enumChan ch = checkContinue0 $ \loop f -> do
-    stream <- liftIO $ readChan ch
-    f stream >>== loop
+responseBS :: ByteString -> Response
+responseBS txt = ResponseBuilder statusOK [("Content-Type", "text/plain")] (fromByteString txt)
 
-runWithChannels :: Protocol p
-                => StreamChan ByteString -> StreamChan ByteString -> (Request -> WebSockets p a) -> IO a
-runWithChannels inChan outChan ws = do
-    r <- run $ enumChan inChan $$
-            runWebSockets defaultWebSocketsOptions ws (iterChan outChan)
-    writeChan outChan EOF
-    either (error . show) return r
+responseNotFound :: Response
+responseNotFound = ResponseBuilder statusNotFound [] (fromByteString "404/resource not found")
 
-data SockjsProtocol = SockjsProtocol
-instance Protocol SockjsProtocol where
-    version _ = "sockjs"
-    headerVersion _ = "sockjs"
-    encodeFrame _ mask (Frame _ _ payload) = fromLazyByteString payload
-    decodeFrame _ = Frame True TextFrame <$> takeLazyByteString
-    finishRequest _ (RequestHttpPart path hs) = return $ Right $ Request path hs response101
-    implementations = [SockjsProtocol]
-instance TextProtocol SockjsProtocol
+responseClosed :: Response
+responseClosed = ResponseBuilder statusNotFound [] (fromByteString "closed")
 
-type SessionId = ByteString
-data Session = Session
-  { sessionId :: SessionId
-  , inChan :: StreamChan ByteString
-  , outChan :: StreamChan ByteString
-  }
+type SessionId = Text
+type Session = (StreamChan ByteString, StreamChan ByteString)
 type SessionMap = Map SessionId Session
 
-type WSApp a = Request -> WebSockets SockjsProtocol a
-type AppRoute = [([ByteString], WSApp a)]
+type WSApp p = WebSockets p ()
+type AppRoute p = [([Text], WSApp p)]
 
-newSockjsSession :: MVar SessionMap -> SessionId -> WSApp a -> IO Session
-newSockjsSession msm sid app = modifyMVar msm $ \sm -> do
+ensureSession :: MVar SessionMap -> SessionId -> WSApp ChanProtocol -> IO Session
+ensureSession msm sid ws = modifyMVar msm $ \sm -> do
     case M.lookup sid sm of
         Just old -> return (sm, old)
         Nothing -> do
-            new <- Session <$> pure sid 
-                           <*> newChan
-                           <*> newChan
+            new <- (,) <$> newChan <*> newChan
             let sm' = M.insert sid new sm
-            forkIO $ runWithChannels (inChan new) (outChan new) app >> return ()
+            forkIO $ runWithChans (fst new) (snd new) ws
             return (sm', new)
 
+httpApp :: MVar SessionMap -> AppRoute ChanProtocol -> Application
+httpApp msm apps req = case msum $ map match apps of
+    Just (app, (_:sid:path)) -> do
+        (inChan, outChan) <- liftIO $ ensureSession msm sid app
+        case path of
+            ["xhr"] -> do
+                s <- liftIO (readChan outChan)
+                case s of
+                    Chunks xs -> return . responseBS . S.concat $ xs
+                    EOF -> return responseClosed
+            ["xhr_send"] -> iterChan inChan >> return (responseBS "")
+            _ -> return $ responseNotFound
+    _ -> return $ responseNotFound
+  where
+    match (prefix, app) = (app, ) <$> stripPrefix prefix (pathInfo req)
 
-wsApp :: AppRoute -> Request -> WebSockets SockjsProtocol a
-wsApp apps req = case msum $ map matchOne apps of
-    Just (app, (_:_:"websocket")) -> do
+wsApp :: AppRoute Hybi00 -> WS.Request -> WebSockets Hybi00 ()
+wsApp apps req = case msum $ map match apps of
+    Just (app, [_,_,"websocket"]) -> do
         acceptRequest req
-        app req
+        app
     _ -> rejectRequest req "Forbidden!"
 
-  where path = S.split '/' (requestPath req)
+  where path = decodePathSegments $ requestPath req
         match (prefix, app) = (app, ) <$> stripPrefix prefix path
 
-echoApp :: Request -> WebSockets SockjsProtocol a
-echoApp req = forever $ do
+echo :: TextProtocol p => WebSockets p ()
+echo = forever $ do
     msg <- receiveData
     sendTextData (msg::L.ByteString)
+
+type ServerState p = MVar (Map Unique (WS.Sink p))
+
+broadcast :: TextProtocol p => ServerState p -> WebSockets p ()
+broadcast st = do
+    identity <- liftIO newUnique
+    sink <- getSink
+    liftIO $ modifyMVar_ st $ return . M.insert identity sink
+    forever $ do
+        msg <- receiveData
+        clients <- filter ((/=identity) . fst) . M.toList <$> liftIO (readMVar st)
+        liftIO $ forM_ clients $ \(_, sink) -> sendSink sink (textData (msg::L.ByteString))
+
+serverState :: TextProtocol p => ServerState p
+serverState = unsafePerformIO $ newMVar M.empty
+
+apps :: TextProtocol p => AppRoute p
+apps = [ ( ["echo"], echo )
+       , ( ["broadcast"], broadcast serverState )
+       ]
+
+-- newApps :: IO (forall p. TextProtocol p => AppRoute p)
+-- newApps = do
+--     st <- newMVar M.empty
+--     return [ ( ["echo"], echo )
+--            , ( ["broadcast"], broadcast st )
+--            ]
+
+main :: IO ()
+main = do
+    -- apps <- newApps
+    msm <- newMVar M.empty
+    runSettings defaultSettings
+           { settingsPort = 8080
+           , settingsIntercept = WaiWS.intercept (wsApp apps)
+           } $ httpApp msm apps
 
