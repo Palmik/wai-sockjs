af3cab9e9ccd9757a2074c88e81f7c9a1df56542
yihuang <yi.codeplayer@gmail.com>
2011-11-15 16:14:57 +0800

    move emulate protocol into websockets.

--- websocket.hs
+++ websocket.hs
@@ -5,39 +5,35 @@ import Data.List (stripPrefix)
 import qualified Data.Map as M
 import Data.Unique
 
+import Data.Attoparsec.Enumerator (iterParser)
 import Data.Text (Text)
-import qualified Data.Text.Encoding as T
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as S
 import qualified Data.ByteString.Lazy as L
 import Data.Enumerator hiding (map, filter)
+import qualified Data.Enumerator.List as EL
 import Blaze.ByteString.Builder
 
-import Control.Exception (throw)
 import Control.Monad.IO.Class
 import Control.Monad
 import Control.Applicative
 import Control.Concurrent
 
 import Network.HTTP.Types
-import Network.Wai
+import Network.Wai hiding (responseLBS)
 import Network.Wai.Handler.Warp (runSettings, defaultSettings, Settings(..))
-import Network.WebSockets hiding (Request, Response)
+import Network.WebSockets hiding (Request, Response, fromLazyByteString)
 import qualified Network.WebSockets as WS
-import Network.WebSockets.Chan
+import Network.WebSockets.Emulate
 import qualified Network.Wai.Handler.WebSockets as WaiWS
-
-responseText :: Text -> Response
-responseText txt = ResponseBuilder statusOK [("Content-Type", "text/plain")] (fromByteString . T.encodeUtf8 $ txt)
 
 responseBS :: ByteString -> Response
 responseBS txt = ResponseBuilder statusOK [("Content-Type", "text/plain")] (fromByteString txt)
 
+responseLBS :: L.ByteString -> Response
+responseLBS txt = ResponseBuilder statusOK [("Content-Type", "text/plain")] (fromLazyByteString txt)
+
 responseNotFound :: Response
 responseNotFound = ResponseBuilder statusNotFound [] (fromByteString "404/resource not found")
-
-responseClosed :: Response
-responseClosed = ResponseBuilder statusNotFound [] (fromByteString "closed")
 
 type SessionId = Text
 type Session = (StreamChan ByteString, StreamChan ByteString)
@@ -46,29 +42,40 @@ type SessionMap = Map SessionId Session
 type WSApp p = WebSockets p ()
 type AppRoute p = [([Text], WSApp p)]
 
-ensureSession :: MVar SessionMap -> SessionId -> WSApp ChanProtocol -> IO Session
-ensureSession msm sid ws = modifyMVar msm $ \sm -> do
+-- | create session if not exists, Left -> old session, Right -> new session.
+ensureSession :: MVar SessionMap -> SessionId -> WSApp EmulateProtocol -> IO (Either Session Session)
+ensureSession msm sid ws = modifyMVar msm $ \sm ->
     case M.lookup sid sm of
-        Just old -> return (sm, old)
+        Just old -> return (sm, Left old)
         Nothing -> do
             new <- (,) <$> newChan <*> newChan
             let sm' = M.insert sid new sm
-            forkIO $ runWithChans (fst new) (snd new) ws
-            return (sm', new)
+            _ <- forkIO $ uncurry runEmulator new ws
+            return (sm', Right new)
 
-httpApp :: MVar SessionMap -> AppRoute ChanProtocol -> Application
+getSession :: MVar SessionMap -> SessionId -> IO (Maybe Session)
+getSession msm sid = M.lookup sid <$> readMVar msm
+
+httpApp :: MVar SessionMap -> AppRoute EmulateProtocol -> Application
 httpApp msm apps req = case msum $ map match apps of
-    Just (app, (_:sid:path)) -> do
-        (inChan, outChan) <- liftIO $ ensureSession msm sid app
-        case path of
-            ["xhr"] -> do
-                s <- liftIO (readChan outChan)
-                case s of
-                    Chunks xs -> return . responseBS . S.concat $ xs
-                    EOF -> return responseClosed
-            ["xhr_send"] -> iterChan inChan >> return (responseBS "")
-            _ -> return $ responseNotFound
-    _ -> return $ responseNotFound
+    Just (app, _:sid:path) -> case path of
+        ["xhr"] ->
+            liftIO (ensureSession msm sid app) >>=
+            either (\(_, outChan) -> do
+                      r <- liftIO $ run $ enumChan outChan $$ iterParser (decodeFrame EmulateProtocol)
+                      either (error . show) (return . responseLBS . framePayload) r
+                   )
+                   (\_ -> return $ responseBS "o\n")
+        ["xhr_send"] ->
+            liftIO (getSession msm sid) >>=
+            maybe (return responseNotFound)
+                  (\(inChan, _) -> do
+                      -- msg <- joinI $ EB.isolate len $$ EL.consume
+                      msg <- EL.consume
+                      liftIO $ writeChan inChan $ Chunks [toByteString $ encodeFrame EmulateProtocol Nothing $ Frame False BinaryFrame $ L.fromChunks msg]
+                      return $ responseBS "" )
+        _ -> return responseNotFound
+    _ -> return responseNotFound
   where
     match (prefix, app) = (app, ) <$> stripPrefix prefix (pathInfo req)
 
@@ -97,18 +104,18 @@ broadcast st = do
     forever $ do
         msg <- receiveData
         clients <- filter ((/=identity) . fst) . M.toList <$> liftIO (readMVar st)
-        liftIO $ forM_ clients $ \(_, sink) -> sendSink sink (textData (msg::L.ByteString))
+        liftIO $ forM_ clients $ \(_, s) -> sendSink s (textData (msg::L.ByteString))
 
 serverState :: TextProtocol p => ServerState p
 serverState = unsafePerformIO $ newMVar M.empty
 
-apps :: TextProtocol p => AppRoute p
-apps = [ ( ["echo"], echo )
+route :: TextProtocol p => AppRoute p
+route = [ ( ["echo"], echo )
        , ( ["broadcast"], broadcast serverState )
        ]
 
--- newApps :: IO (forall p. TextProtocol p => AppRoute p)
--- newApps = do
+-- newRoute :: IO (forall p. TextProtocol p => AppRoute p)
+-- newRoute = do
 --     st <- newMVar M.empty
 --     return [ ( ["echo"], echo )
 --            , ( ["broadcast"], broadcast st )
@@ -116,10 +123,10 @@ apps = [ ( ["echo"], echo )
 
 main :: IO ()
 main = do
-    -- apps <- newApps
+    -- route <- newRoute
     msm <- newMVar M.empty
     runSettings defaultSettings
            { settingsPort = 8080
-           , settingsIntercept = WaiWS.intercept (wsApp apps)
-           } $ httpApp msm apps
+           , settingsIntercept = WaiWS.intercept (wsApp route)
+           } $ httpApp msm route
 
